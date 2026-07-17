ed162ee61ddd1cf89e49c3a3473957fbc88aaf5f
Mikey Diamond <mikeydiamond@thinkhuge.net>
2026-07-17 11:08:59 +0700

    updating deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://kingschair.co.uk",
+  site: "https://dizzlacus.github.io",
+  base: "/kings-chair",
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
